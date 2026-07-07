df786f89a00a32475596409346ff5122f9828593
Simran Kaushal <kaushalsimran620@gmail.com>
2026-07-07 14:25:45 +0530

    fix: resolve casing mismatch for User model on Linux VM

--- Backend/src/auth/authRoutes.js
+++ Backend/src/auth/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios'); 
-const User = require('../models/User'); 
+const User = require('../models/user'); 
 const Driver = require('../models/Driver');
 const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY; 
 
